549bb0dee559ef6e02e2e7395f7548f75cd0cea1
Aliaksandr Klimianok <aliaksandr_klimianok@epam.com>
2023-01-12 13:51:36 +0300

    added it-shop html Template

---
+++ it-shop/data/categories.js
@@ -0,0 +1,74 @@
+// https://api.escuelajs.co/api/v1/categories
+
+export const CATEGORIES = [
+  {
+    id: 1,
+    name: "Clothes",
+    image: "https://api.lorem.space/image/fashion?w=640&h=480&r=7742",
+    creationAt: "2023-01-12T00:47:59.000Z",
+    updatedAt: "2023-01-12T00:47:59.000Z",
+  },
+  {
+    id: 2,
+    name: "Electronics",
+    image: "https://api.lorem.space/image/watch?w=640&h=480&r=5692",
+    creationAt: "2023-01-12T00:47:59.000Z",
+    updatedAt: "2023-01-12T00:47:59.000Z",
+  },
+  {
+    id: 3,
+    name: "Titulo 3 Cambiado",
+    image: "https://api.lorem.space/image/furniture?w=640&h=480&r=2451",
+    creationAt: "2023-01-12T00:47:59.000Z",
+    updatedAt: "2023-01-12T02:50:04.000Z",
+  },
+  {
+    id: 4,
+    name: "Shoes",
+    image: "https://api.lorem.space/image/shoes?w=640&h=480&r=1623",
+    creationAt: "2023-01-12T00:47:59.000Z",
+    updatedAt: "2023-01-12T00:47:59.000Z",
+  },
+  {
+    id: 5,
+    name: "Others",
+    image: "https://api.lorem.space/image?w=640&h=480&r=574",
+    creationAt: "2023-01-12T00:47:59.000Z",
+    updatedAt: "2023-01-12T00:47:59.000Z",
+  },
+  {
+    id: 7,
+    name: "New Category",
+    image: "https://placeimg.com/640/480/any",
+    creationAt: "2023-01-12T02:45:17.000Z",
+    updatedAt: "2023-01-12T02:45:17.000Z",
+  },
+  {
+    id: 8,
+    name: "New Category",
+    image: "https://placeimg.com/640/480/any",
+    creationAt: "2023-01-12T04:58:14.000Z",
+    updatedAt: "2023-01-12T04:58:14.000Z",
+  },
+  {
+    id: 9,
+    name: "Nueva categoria",
+    image: "https://placeimg.com/640/480/any",
+    creationAt: "2023-01-12T07:54:08.000Z",
+    updatedAt: "2023-01-12T07:54:08.000Z",
+  },
+  {
+    id: 10,
+    name: "Nueva categoria",
+    image: "https://placeimg.com/640/480/any",
+    creationAt: "2023-01-12T07:54:57.000Z",
+    updatedAt: "2023-01-12T07:54:57.000Z",
+  },
+  {
+    id: 11,
+    name: "Nueva categoria",
+    image: "https://placeimg.com/640/480/any",
+    creationAt: "2023-01-12T07:55:00.000Z",
+    updatedAt: "2023-01-12T07:55:00.000Z",
+  },
+];
